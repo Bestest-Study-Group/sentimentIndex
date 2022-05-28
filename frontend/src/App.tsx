0b7c285f5a186aa9274b3ca990f09c2a807be833
Colin Lin <colincool100@gmail.com>
2022-05-28 17:22:00 -0400

    connect frontend to rest api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import Header from "./components/header/header"
-
+import Header from "./components/header/header";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div>
-    <Header sentiment="neutral" />
-  </div>;
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: process.env.REACT_APP_REST_API + "/api/reddit",
+    }).then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+  return (
+    <div>
+      <Header sentiment="neutral" />
+    </div>
+  );
 }
 
 export default App;
